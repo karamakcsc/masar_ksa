ba62a1b0ab5512741f4fe4e5c3e3facc6c3a4ed6
karamakcsc <karamakcsc@hotmail.com>
2023-04-19 12:25:25 +0300

    edite mandatory1

--- masar_ksa/masar_ksa/report/sales_tax_invoice/sales_tax_invoice.js
+++ masar_ksa/masar_ksa/report/sales_tax_invoice/sales_tax_invoice.js
@@ -1,65 +1,65 @@
-// // Copyright (c) 2023, KCSC and contributors
-// // For license information, please see license.txt
-// /* eslint-disable */
-//
-// frappe.query_reports["Sales TAX Invoice"] = {
-// 	"filters": [
-// 		{
-// 			"fieldname": "si_no",
-// 			"label": __("Sales Invoice"),
-// 			"fieldtype": "Link",
-// 			"options": "Sales Invoice",
-// 			"width": 100,
-// 			"reqd": 0,
-// 		},
-// 		{
-// 			"fieldname": "from",
-// 			"label": __("From Date"),
-// 			"fieldtype": "Date",
-// 			"width": 80,
-// 			"reqd": 1,
-// 			"default": dateutil.year_start()
-// 		 },
-// 		 {
-// 			"fieldname": "to",
-// 			"label": __("To Date"),
-// 			"fieldtype": "Date",
-// 			"width": 80,
-// 			"reqd": 1,
-// 			"default": dateutil.year_end()
-// 		},
-// 	  {
-// 			"fieldname": "customer_name",
-// 			"label": __("Customer Name"),
-// 			"fieldtype": "Link",
-// 			"options": "Customer",
-// 			"width": 100,
-// 			"reqd": 0,
-// 		},
-// 		// {
-// 		// 	"fieldname": "account",
-// 		// 	"label": __("Account Name"),
-// 		// 	"fieldtype": "Link",
-// 		// 	"options": "Account",
-// 		// 	"width": 100,
-// 		// 	"reqd": 0,
-// 		// },
-// 		{
-// 			"fieldname": "tax_category",
-// 			"label": __("Tax Group"),
-// 			"fieldtype": "Link",
-// 			"options": "Tax Category",
-// 			"width": 100,
-// 			"reqd": 0,
-// 		},
-// 		{
-// 							"fieldname": "is_return",
-// 							"label": __("Is Return"),
-// 							"fieldtype": "Check",
-// 							//"options": "Is Return",
-// 							"width": 100,
-// 							"reqd": 0,
-// 						}
-//
-// ]
-// };
+// Copyright (c) 2023, KCSC and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Sales TAX Invoice"] = {
+	"filters": [
+		// {
+		// 	"fieldname": "si_no",
+		// 	"label": __("Sales Invoice"),
+		// 	"fieldtype": "Link",
+		// 	"options": "Sales Invoice",
+		// 	"width": 100,
+		// 	"reqd": 0,
+		// },
+		// {
+		// 	"fieldname": "from",
+		// 	"label": __("From Date"),
+		// 	"fieldtype": "Date",
+		// 	"width": 80,
+		// 	"reqd": 1,
+		// 	"default": dateutil.year_start()
+		//  },
+		//  {
+		// 	"fieldname": "to",
+		// 	"label": __("To Date"),
+		// 	"fieldtype": "Date",
+		// 	"width": 80,
+		// 	"reqd": 1,
+		// 	"default": dateutil.year_end()
+		// },
+	  // {
+		// 	"fieldname": "customer_name",
+		// 	"label": __("Customer Name"),
+		// 	"fieldtype": "Link",
+		// 	"options": "Customer",
+		// 	"width": 100,
+		// 	"reqd": 0,
+		// },
+		// // {
+		// // 	"fieldname": "account",
+		// // 	"label": __("Account Name"),
+		// // 	"fieldtype": "Link",
+		// // 	"options": "Account",
+		// // 	"width": 100,
+		// // 	"reqd": 0,
+		// // },
+		// {
+		// 	"fieldname": "tax_category",
+		// 	"label": __("Tax Group"),
+		// 	"fieldtype": "Link",
+		// 	"options": "Tax Category",
+		// 	"width": 100,
+		// 	"reqd": 0,
+		// },
+		// {
+		// 					"fieldname": "is_return",
+		// 					"label": __("Is Return"),
+		// 					"fieldtype": "Check",
+		// 					//"options": "Is Return",
+		// 					"width": 100,
+		// 					"reqd": 0,
+		// 				}
+
+]
+};
